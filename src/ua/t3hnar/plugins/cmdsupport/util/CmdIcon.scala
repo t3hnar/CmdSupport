cce8b10e3112d74100782b0adf1908a5df921e2b
Yaroslav Klymko <t3hnar@gmail.com>
2014-10-19 23:00:05 +0300

    add new icon

--- src/ua/t3hnar/plugins/cmdsupport/util/CmdIcon.scala
+++ src/ua/t3hnar/plugins/cmdsupport/util/CmdIcon.scala
@@ -2,13 +2,7 @@ package ua.t3hnar.plugins.cmdsupport.util
 
 import com.intellij.openapi.util.IconLoader
 
-/**
- * @author Yaroslav Klymko aka t3hnar
- */
-
 object CmdIcon {
-
-	private val path = "/icons/"
-	val file = IconLoader.findIcon(path + "cmd-16.png")
+  val file = IconLoader.findIcon("/icons/cmd.png")
 }
 
